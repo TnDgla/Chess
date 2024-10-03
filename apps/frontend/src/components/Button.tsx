125e538a3f5fa1ef41203be8cc6a2712b01599e6
shubham yadav <126192924+yadavshubham01@users.noreply.github.com>
2024-10-03 13:18:29 +0530

    Add buttoncolor

--- apps/frontend/src/components/Button.tsx
+++ apps/frontend/src/components/Button.tsx
@@ -1,3 +1,7 @@
+import { THEMES_DATA } from '@/constants/themes';
+import { ThemeContext } from '@/context/themeContext';
+import { useContext, useEffect, useState } from 'react';
+
 export const Button = ({
   onClick,
   children,
@@ -7,12 +11,41 @@ export const Button = ({
   children: React.ReactNode;
   className?: string;
 }) => {
+  const [buttonColor, setButtonColor] = useState<string>('');
+  const themeContext = useContext(ThemeContext);
+  useEffect(() => {
+    if (themeContext) {
+      const currTheme = themeContext.theme;
+      const theme = THEMES_DATA.find((theme) => theme.name === currTheme);
+      if (theme) {
+        switch (theme.name) {
+          case 'Blue':
+            setButtonColor(theme.buttonColor);
+            break;
+          case 'Red':
+            setButtonColor(theme.buttonColor);
+            break;
+          case 'Violet':
+            setButtonColor(theme.buttonColor);
+            break;
+          case 'bubblegum':
+            setButtonColor(theme.buttonColor);
+            break;
+          case 'default':
+            setButtonColor(theme.buttonColor);
+        }
+      }
+    }
+  }, []);
   return (
     <button
+      style={{ backgroundColor: buttonColor }}
       onClick={onClick}
-      className={`px-8 py-4 text-2xl bg-green-500 text-white font-bold rounded ${className}`}
+      className={`px-8 py-4 text-2xl text-white font-bold rounded ${className}`}
     >
       {children}
     </button>
   );
 };
+
+// Define the atom with the correct type
--- apps/frontend/src/constants/themes.ts
+++ apps/frontend/src/constants/themes.ts
@@ -1,29 +1,63 @@
-import { THEME } from "@/context/themeContext"
+import { THEME } from '@/context/themeContext';
 
-type THEME_DATA = {
-  id: number,
-  name: THEME,
-  background: string,
-  "board-light": string,
-  "board-dark": string,
-  "board-image": string
-}
+export type THEME_DATA = {
+  id: number;
+  name: THEME;
+  background: string;
+  buttonColor: string;
+  'board-light': string;
+  'board-dark': string;
+  'board-image': string;
+};
 
 export const THEMES_DATA: THEME_DATA[] = [
   {
     id: 1,
-    name: "default",
-    background: "#302E2B",
-    "board-light": "#EBECD0",
-    "board-dark": "#739552",
-    "board-image": "https://www.chess.com/bundles/web/images/offline-play/standardboard.1d6f9426.png",
+    name: 'default',
+    background: '#302E2B',
+    buttonColor: '#16a34a',
+    'board-light': '#EBECD0',
+    'board-dark': '#739552',
+    'board-image': 'https://www.chess.com/bundles/web/images/offline-play/standardboard.1d6f9426.png',
   },
   {
     id: 2,
-    name: "bubblegum",
-    background: "#E6455E",
-    "board-light": "#FEFFFE",
-    "board-dark": "#FBD9E1",
-    "board-image": "https://res.cloudinary.com/dcugqfvvg/image/upload/e_improve,e_sharpen/v1718047051/screenshot-localhost_5173-2024.06.11-00_44_01_pxwr43.png",
-  }
-]
+    name: 'bubblegum',
+    background: '#E6455E',
+    buttonColor: '#FBD9E1',
+    'board-light': '#FEFFFE',
+    'board-dark': '#FBD9E1',
+    'board-image':
+      'https://res.cloudinary.com/dcugqfvvg/image/upload/e_improve,e_sharpen/v1718047051/screenshot-localhost_5173-2024.06.11-00_44_01_pxwr43.png',
+  },
+  {
+    id: 3,
+    name: 'Blue',
+    background: '#030712',
+    buttonColor: '#164e63',
+    'board-light': '#cffafe',
+    'board-dark': '#164e63',
+    'board-image':
+      'https://thumbs.dreamstime.com/b/chess-board-blue-colors-chess-pieces-vector-illustration-241711727.jpg',
+  },
+  {
+    id: 4,
+    name: 'Violet',
+    background: '#6b7280',
+    buttonColor: '#3b0764',
+    'board-light': '#d8b4fe',
+    'board-dark': '#4338ca',
+    'board-image':
+      'https://thumbs.dreamstime.com/b/chess-board-blue-colors-chess-pieces-vector-illustration-241711727.jpg',
+  },
+  {
+    id: 5,
+    name: 'Red',
+    background: '#fda4af',
+    buttonColor: '#e11d48',
+    'board-light': '#fecdd3',
+    'board-dark': '#e11d48',
+    'board-image':
+      'https://thumbs.dreamstime.com/b/chess-board-blue-colors-chess-pieces-vector-illustration-241711727.jpg',
+  },
+];
